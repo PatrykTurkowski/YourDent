aba9006e3ce1df88fddd47eb0f9f7de4092ed3b8
patryk <patryk.turkowski21@gmail.com>
2022-12-14 00:52:57 +0100

    change vite file app.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/script.js',
+                'resources/js/app.js',
                 // 'resources/js/users.js',
                 // 'resources/js/categories.js',
                 // 'resources/js/services.js',
