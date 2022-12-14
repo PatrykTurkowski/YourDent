116de4812d750ea3f0551c8b9762a9f8531cd3d8
patryk <patryk.turkowski21@gmail.com>
2022-12-14 19:04:47 +0100

    try change mail without vite

--- resources/views/emails/reserve_visit.blade.php
+++ resources/views/emails/reserve_visit.blade.php
@@ -1,13 +1,7 @@
-@extends('layouts.main')
-@section('links')
-    @include('includes._links')
-@endsection
-@section('content')
-    <h1 class="mb-5 text-center">Dzień dobry, {{ $user->name }}</h1>
-    <p class="">Udało Ci się zarezerwować następujący termin: {{ $term->date_visit }} o godzinie:
-        {{ $term->start_visit }}</p>
-    <p class="">Pomocy udzieli Ci: dr {{ $term->doctors->name }}</p>
+<h1 class="mb-5 text-center">Dzień dobry, {{ $user->name }}</h1>
+<p class="">Udało Ci się zarezerwować następujący termin: {{ $term->date_visit }} o godzinie:
+    {{ $term->start_visit }}</p>
+<p class="">Pomocy udzieli Ci: dr {{ $term->doctors->name }}</p>
 
-    <p class=" mt-4">Z pozdrowieniami YourDent</p>
-    <p class="tent-muted mt-4">Wiadomość została wygenerowana automatycznie, nie odpowiadaj na nią.</p>
-@endsection
+<p class=" mt-4">Z pozdrowieniami YourDent</p>
+<p class="tent-muted mt-4">Wiadomość została wygenerowana automatycznie, nie odpowiadaj na nią.</p>
