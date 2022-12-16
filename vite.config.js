79fc313c5b3c400541c22baa3f947a9b033e3d65
patryk <patryk.turkowski21@gmail.com>
2022-12-16 18:36:30 +0100

    swap vite file

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
+                'resources/sass/admin.scss',
                 'resources/js/script.js',
                 'resources/js/app.js',
                 // 'resources/js/users.js',
